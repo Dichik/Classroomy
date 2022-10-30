2467b1296123d5fc5492b0ef9275e19e43dd37b0
Dichik <omeluan.dima@gmail.com>
2022-10-30 19:19:24 +0200

    init commit

---
+++ src/main/java/com/main/classroomy/ClassroomyApplication.java
@@ -0,0 +1,13 @@
+package com.main.classroomy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassroomyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassroomyApplication.class, args);
+    }
+
+}
