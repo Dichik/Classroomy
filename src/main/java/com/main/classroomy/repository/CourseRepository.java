d13ce665f67259d18fde7fec65252fa97c21849c
Dichik <omeluan.dima@gmail.com>
2022-11-06 17:56:41 +0200

    Update course logic

---
+++ src/main/java/com/main/classroomy/repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.main.classroomy.repository;
+
+import com.main.classroomy.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+}
---
+++ src/main/java/com/main/classroomy/service/CourseService.java
@@ -0,0 +1,17 @@
+package com.main.classroomy.service;
+
+import com.main.classroomy.repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourseService {
+
+    private final CourseRepository courseRepository;
+
+    @Autowired
+    public CourseService(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+}
