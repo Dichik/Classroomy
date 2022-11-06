41af66df524f4c5b811b8ca4a20246d7c1c639d2
Dichik <omeluan.dima@gmail.com>
2022-11-06 17:57:03 +0200

    Update Teacher logic

---
+++ src/main/java/com/main/classroomy/service/TeacherService.java
@@ -0,0 +1,17 @@
+package com.main.classroomy.service;
+
+import com.main.classroomy.repository.TeacherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TeacherService {
+
+    private final TeacherRepository teacherRepository;
+
+    @Autowired
+    public TeacherService(TeacherRepository teacherRepository) {
+        this.teacherRepository = teacherRepository;
+    }
+
+}
