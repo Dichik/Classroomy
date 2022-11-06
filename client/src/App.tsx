4aa9c174b8467de81f5be72ec757adbfbf6f7115
Dichik <omeluan.dima@gmail.com>
2022-11-06 19:47:02 +0200

    Add base main page with Hello, World

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-	return <div>Hello world!</div>;
+	// eslint-disable-next-line
+	const [petitions, setPetitions] = useState([]);
+
+	return (
+		<div>
+			<header className="header-class"></header>
+			<h1 className="title-name">Hello world!</h1>
+			<footer className="footer-class"></footer>
+		</div>
+	);
 }
 
 export default App;
