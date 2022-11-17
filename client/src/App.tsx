709d731034b37d370ce3508ecf5b80c9d60c83d5
Dichik <omeluan.dima@gmail.com>
2022-11-17 15:57:13 +0200

    Update app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-	// eslint-disable-next-line
-	const [petitions, setPetitions] = useState([]);
+  // eslint-disable-next-line
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-	// TODO should have refresh button
-	// TODO should have welcome page without courses
-	// TODO auth page to do firstly
+  // TODO should have refresh button
+  // TODO should have welcome page without courses
+  // TODO auth page to do firstly
 
-	return (
-		<div>
-			<header className="header-class"></header>
-			<h1 className="title-name">Hello world!</h1>
-			<footer className="footer-class"></footer>
-		</div>
-	);
+  useEffect(() => {
+    setLoading(true);
+
+    fetch("api/courses")
+      .then((response) => response.json())
+      .then((data) => {
+        setCourses(data);
+        setLoading(false);
+        console.log(courses);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <header className="header-class"></header>
+      <h1 className="title-name">Hello world!</h1>${courses}
+      <footer className="footer-class"></footer>
+    </div>
+  );
 }
 
 export default App;
